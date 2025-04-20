a22d47ca148cd46671b8ccf636d02b6581505838
irsals07 <145377188+irsals07@users.noreply.github.com>
2025-04-20 19:25:29 -0400

    Add files via upload

---
+++ Views/CameraView.swift
@@ -0,0 +1,16 @@
+//
+//  SignupView.swift
+//  FindItApp
+//
+//  Created by Aaron Siam on 4/20/25.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct CameraView: View {
+    var body: some View {
+        Text("Camera UI goes here")
+            .font(.headline)
+    }
+}
